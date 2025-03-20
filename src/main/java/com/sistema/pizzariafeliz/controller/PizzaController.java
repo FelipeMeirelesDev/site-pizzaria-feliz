3ceb7b7049482e24501d0277aaed11329448a6fe
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2025-03-20 08:57:01 -0300

    Adicionando os controladores
    Adicionando as classes de controladores (Controllers/APIs REST).

---
+++ src/main/java/com/sistema/pizzariafeliz/controller/PizzaController.java
@@ -0,0 +1,26 @@
+package com.sistema.pizzariafeliz.controller;
+
+import com.sistema.pizzariafeliz.model.Pizza;
+import com.sistema.pizzariafeliz.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pizzas")
+public class PizzaController {
+    @Autowired
+    private PizzaService pizzaService;
+
+    @GetMapping
+    public List<Pizza> listarPizzas() {
+        return pizzaService.listarPizzas();
+    }
+
+    @PostMapping
+    public Pizza salvarPizza(@RequestBody Pizza pizza) {
+        return pizzaService.salvarPizza(pizza);
+    }
+}
+
