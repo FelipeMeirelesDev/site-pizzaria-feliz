bb4a1ffd102c72f40cefb8d19f54c6625b5429aa
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2025-03-21 14:51:12 -0300

    adicionando as funções
    adicionando as funções de requisições do pedido e estilizando os botões.

--- src/main/java/com/sistema/pizzariafeliz/controller/PedidoController.java
+++ src/main/java/com/sistema/pizzariafeliz/controller/PedidoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vendas")
+@RequestMapping("/pedidos")
 public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
