72e4cb5b141bafff408123b93035f83ebc833c0f
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2025-03-29 20:22:14 -0300

    teste

--- src/main/resources/static/scripts/cartoes.js
+++
@@ -1,4 +0,0 @@
-function setPreco(valor) {
-    localStorage.setItem('preco', valor);  // Salva o valor no localStorage
-    window.location.href = 'formvenda.html';  // Redireciona para o formulário
-}
